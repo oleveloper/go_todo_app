e4fd365edcf8f12c2d8fa20a5083eeff9e424682
oleveloper <oleveloper@gmail.com>
2024-03-27 16:54:59 +0900

    Refactor main function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/oleveloper/go_todo_app
 
 go 1.19
+
+require golang.org/x/sync v0.6.0 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func run(context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
+	}
+	s.ListenAndServe()
+
+	return nil
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
 	}
 }
